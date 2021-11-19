5a89d2e94f6a5e562caffd0b85e30a8b359f3a9b
Vinith-arun <vinith.p@tringapps.com>
2021-11-19 10:03:41 +0530

    firstcommit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,11 @@
+var bv=true
+function tb(){
+bv ? false:true;
+console.log(bv)
+}
 function Header(){
     return(
-        <header>Header</header>
+        <header>Header<button onClick={tb} id="but"></button> </header>
     )
 }
 export default Header;
